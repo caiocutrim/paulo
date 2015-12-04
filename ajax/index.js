1d6b4af4a1c3ec8279da3bbbbea8ca61a7248c62
Caio Cutrim <kaiocoutrim@gmail.com>
2015-12-04 17:32:37 -0300

    added files

---
+++ ajax/index.js
@@ -0,0 +1,10 @@
+/**
+ * O que precisamos fazer:
+ * criar uma instancia de XMLHttpRequest
+ * abri uma conexão para localhost:rota-a-ser-consumida
+ * send(null) no final, para não enviarmos nada para o servidor
+ * a nossa request vai ser do tipo get, trazendo dados do servidor
+ * verificar os status da request
+ * fazer alguma coisa <3
+ * :)
+ */
